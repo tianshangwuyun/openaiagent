909ad6bdbe08036d1df66319a38422c2f9f9d73e
tianshangwuyun <281139349@qq.com>
2025-07-11 15:38:00 +0800

    first

--- src/agents.ts
+++ src/agents.ts
@@ -1,5 +1,17 @@
 import { Agent, tool } from '@openai/agents';
+import { aisdk } from '@openai/agents-extensions';
+import { createOpenAI } from '@ai-sdk/openai';
+
 import z from 'zod';
+
+
+const openaiClient = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+  baseURL: process.env.OPENAI_API_BASE_URL,
+});
+
+const model = aisdk(openaiClient(process.env.OPENAI_MODEL!));
+
 
 const getWeather = tool({
   name: 'getWeather',
@@ -18,4 +30,5 @@ export const agent = new Agent({
   name: 'Basic Agent',
   instructions: 'You are a basic agent.',
   tools: [getWeather],
+  model,
 });
